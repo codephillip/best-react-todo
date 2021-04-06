de5d51abca45d930f98835ac2bc5a8fc33e6d87a
codephillip <codephillip@gmail.com>
2021-04-06 16:10:04 +0300

    added header and footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import './Footer.css'
+
+const Footer = () => (
+    <footer>
+        <span className="copyright">&copy; Copy right</span>
+    </footer>
+);
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import './Header.css'
+
+const Header = () => (
+    <header>
+        <h1 className="titletext">Best Todo App</h1>
+    </header>
+);
+
+export default Header;
