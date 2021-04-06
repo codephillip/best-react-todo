8ae8e6f8f8d14ccf69cdae84431175a676307456
codephillip <codephillip@gmail.com>
2021-04-06 16:18:59 +0300

    added body template

--- src/hoc/App.js
+++ src/hoc/App.js
@@ -2,14 +2,16 @@ import logo from '../logo.svg';
 import './App.css';
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer"
+import Body from "../components/Body/Body";
 
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Footer/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Body/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
